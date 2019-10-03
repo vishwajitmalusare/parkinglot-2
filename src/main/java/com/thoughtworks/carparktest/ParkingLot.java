202747dab2c4c05a5d143bf0003dd2d9d9e129e2
vishwajeetmalusare <vishwajitmalusare@gmail.com>
2019-10-03 15:49:58 +0530

    [swapnil | vishwajeet] Add. car not park when parking lot has no space

--- src/main/java/com/thoughtworks/carparktest/ParkingLot.java
+++ src/main/java/com/thoughtworks/carparktest/ParkingLot.java
@@ -3,12 +3,27 @@ package com.thoughtworks.carparktest;
 public class ParkingLot {
 
     private final int capacity;
+    private int spaceAvailable;
 
     ParkingLot(int capacity) {
-        this.capacity = 0;
+        this.capacity = capacity;
+        this.spaceAvailable = capacity;
     }
 
     public boolean park(Object object) {
-        return true;
+        if (spaceAvailable > 0) {
+            spaceAvailable--;
+            return true;
+        }
+        return false;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "ParkingLot{" +
+                "capacity=" + capacity +
+                '}';
     }
 }
--- src/test/java/com/thoughtworks/carparktest/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/carparktest/ParkingLotTest.java
@@ -2,13 +2,21 @@ package com.thoughtworks.carparktest;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     @Test
     void givenParkingLotHasCapacity_WhenPark_ThenShouldPark() {
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot(1 ); //this represent available lots
 
         assertTrue(parkingLot.park(new Object()));
+    }
+
+    @Test
+    void givenParkingLotIsFull_WhenPark_ThenShouldNotPark() {
+        ParkingLot parkingLot = new ParkingLot(1); // spaceAvailable = 1
+        parkingLot.park(new Object()); // spaceAvailable--
+
+        assertFalse(parkingLot.park(new Object()));
     }
 }
